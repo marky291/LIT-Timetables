25d37b52bf7310e7642c27c811a37f573092ff6c
Mark Hester <marky360@live.ie>
2021-01-21 00:45:26 +0000

    Recent & Favorite Icons.

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -71,7 +71,7 @@ x-on:search.window="open = true; $nextTick(() => $refs.searchbar.focus())"
                                         <div wire:key="search-{{ $loop->index }}" class="flex items-center justify-between rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white" :class="{ 'bg-indigo-500 text-white': selected === {{$i}} }">
                                             <div class="flex flex-1 items-center py-4 pl-4" wire:click="click('{{ addslashes($model->searchable::class) }}', '{{ $model->searchable->id }}', '{{ $model->searchable->route }}')">
                                                 <div class="mr-2">
-                                                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+                                                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                                 </div>
                                                 <div class="flex items-center h-8">
                                                     <p class="font-semibold overflow-ellipsis whitespace-nowrap">{{ $model->searchable->routeTitle }}</p>
@@ -96,7 +96,7 @@ x-on:search.window="open = true; $nextTick(() => $refs.searchbar.focus())"
                                             <div wire:key="search-{{ $loop->index }}" class="flex items-center justify-between rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white" :class="{ 'bg-indigo-500 text-white': selected === {{$i}} }">
                                                 <div class="flex flex-1 items-center py-4 pl-4" wire:click="click('{{ addslashes($model->searchable::class) }}', '{{ $model->searchable->id }}', '{{ $model->searchable->route }}')">
                                                     <div class="mr-2">
-                                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
+                                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
                                                     </div>
                                                     <div class="flex items-center h-8">
                                                         <p class="font-semibold overflow-ellipsis whitespace-nowrap">{{ $model->searchable->routeTitle }}</p>
