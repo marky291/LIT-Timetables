b54b0500c86c627249988a6be1f6093216a60eef
Mark Hester <marky360@live.ie>
2021-02-16 12:12:22 +0000

    Timetable retrievals are now threaded #20

--- app/Timetable/Commands/SyncTimetableCommand.php
+++ app/Timetable/Commands/SyncTimetableCommand.php
@@ -25,6 +25,9 @@ use App\Timetable\Exceptions\UnknownCourseLocationException;
 use App\Timetable\Parsers\ParseCourseName;
 use App\Timetable\TimetableLinkCreator;
 use App\Timetable\HttpTimetableRequests;
+use App\Timetable\Jobs\CheckScheduleChanges;
+use App\Timetable\Jobs\CollectCourseSchedules;
+use App\Timetable\Jobs\InspectSchedule;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -110,12 +113,9 @@ class SyncTimetableCommand extends Command
         $filter->courses()->unique('slug')->each(function($value) use (&$output) {
             $data = (new ParseCourseName($value->name));
             try {
-                $dept = Department::firstWhere('filter', $value->filter);
-                $camp = Campus::firstOrCreate(['location' => $data->getLocation()]);
-
                 Course::firstOrCreate(['identifier' => $data->getIdentifier()], array_merge($data->toArray(), [
-                    'campus_id' => $camp->id,
-                    'department_id' => $dept->id,
+                    'campus_id' => Campus::firstOrCreate(['location' => $data->getLocation()])->id,
+                    'department_id' => Department::firstWhere('filter', $value->filter)->id,
                 ]));
              } catch (UnknownCourseLocationException $e) {
                 Log::error("Missing course data in course title {$value->name}.");
@@ -141,88 +141,12 @@ class SyncTimetableCommand extends Command
         $course = Course::all();
         $output->progressStart($course->count());
         $course->each(function(Course $course) use ($output) {
-            try
-            {
-                /**
-                 * A custom request class that will track outgoing requests
-                 * to the timetable on the third party service.
-                 * This fires a http request to the third party service.
-                 */
-                $data = $this->http->crawl($course);
-
-                /**
-                 * Check for changes between the previously snapchat of the html
-                 * to the current snapshot of the html, we can differentiate
-                 * the changes to determine the changes in key and value
-                 * and fire an event to let the subscribed users know.
-                 */
-                $mined = $data->get('schedules')->toArray();
-                $stored = optional($course->requests()->latest()->first())->mined;
-                if (($stored && $mined) && ($stored != $mined)) {
-                    for ($i = 0; $i < count($mined); $i++) {
-                        $difference = array_diff_assoc($mined[$i], $stored[$i]);
-                        if ($difference) {
-                            event(new TimetableWasChanged($course));
-                            foreach (array_keys($difference) as $key) {
-                                $event = match ($key) {
-                                    'lecturer' => ScheduledLecturerHasChanged::class,
-                                    'room' => ScheduledRoomHasChanged::class,
-                                    'module' => ScheduledModuleHasChanged::class,
-                                    'day_of_week' => ScheduledDayHasChanged::class,
-                                    'type' => ScheduledTypeHasChanged::class,
-                                    'ending_time', 'starting_time' => ScheduledTimeHasChanged::class,
-                                };
-                                event(new $event($course, $stored, $mined));
-                            }
-                        }
-                    }
-                }
-
-                /**
-                 * Get the academic week of the incoming request data.
-                 */
-                $academic_week = (int) Regex::match('/(?<=Weeks selected for output: )(.*)(?= \(\d)/', $data['meta']['week'])->result();
-
-                /**
-                 * Delete the schedules for this week so we can recreate the indexing of the schedules.
-                 * in the next code block.
-                 */
-                $course->schedules()->where('academic_week', $academic_week)->delete();
-
-                /**
-                 * Lastly we should storage store the schedules belongs to the
-                 * course in selected by the loop.
-                 */
-                $data->get('schedules')->each(function ($attribute) use ($course, $data, $academic_week)
-                {
-                    $carbon = DateConversion::timetableScheduleToCarbon($attribute, $data['meta']);
-                    $module = Module::firstOrCreate(['name' => $attribute['module']]);
-                    $room   = Room::firstOrCreate(['door' => $attribute['room']]);
-                    $type   = Type::firstOrCreate(['abbreviation' => $attribute['type']]);
-
-                    $schedule = $course->schedules()->firstOrCreate([
-                        'starting_date' => $carbon['start'],
-                        'ending_date' => $carbon['end'],
-                        'module_id' => $module->id,
-                        'academic_week' => $academic_week,
-                        'room_id' => $room->id,
-                        'type_id' => $type->id,
-                    ]);
-
-                    /**
-                     * Remove previous lecturers associated with the schedule.
-                     * and re-sync the collected from the new dataset.
-                     */
-                    $schedule->lecturers()->sync(collect(explode(', ', $attribute['lecturer']))
-                        ->map(fn($name) => Lecturer::firstOrCreate(['fullname' => $name])->id)
-                        ->toArray());
-                });
-            } catch (ReturnedBadResponseException $e) {
-                Log::error($e->getMessage());
-            } finally {
-                $output->progressAdvance();
-            }
+            InspectSchedule::dispatch($course);
+            $output->progressAdvance(1);
         });
+
+        $this->comment("Schedules have been dispatched successfully to threads.");
+        $this->info("Done.");
 
         /**
          * Let the command know it succeeded.
