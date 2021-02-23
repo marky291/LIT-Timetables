cad6f03d6d2f0904e0e9ca33bb9e5a72dafae1b7
Mark Hester <marky291@users.noreply.github.com>
2021-02-23 10:51:49 +0000

    Apply fixes from StyleCI

--- app/Timetable/Commands/SyncTimetableCommand.php
+++ app/Timetable/Commands/SyncTimetableCommand.php
@@ -5,34 +5,14 @@ namespace App\Timetable\Commands;
 use App\Models\Campus;
 use App\Models\Course;
 use App\Models\Department;
-use App\Models\Lecturer;
-use App\Models\Module;
-use App\Models\Room;
-use App\Models\Schedule;
-use App\Models\Type;
-use App\Timetable\DateConversion;
-use App\Timetable\Parsers\ParseFilters;
-use App\Timetable\Events\ScheduledDayHasChanged;
-use App\Timetable\Events\ScheduledLecturerHasChanged;
-use App\Timetable\Events\ScheduledModuleHasChanged;
-use App\Timetable\Events\ScheduledRoomHasChanged;
-use App\Timetable\Events\ScheduledTimeHasChanged;
-use App\Timetable\Events\ScheduledTypeHasChanged;
-use App\Timetable\Events\TimetableFetchFailed;
-use App\Timetable\Events\TimetableWasChanged;
-use App\Timetable\Exceptions\ReturnedBadResponseException;
 use App\Timetable\Exceptions\UnknownCourseLocationException;
-use App\Timetable\Parsers\ParseCourseName;
-use App\Timetable\TimetableLinkCreator;
 use App\Timetable\HttpTimetableRequests;
-use App\Timetable\Jobs\CheckScheduleChanges;
-use App\Timetable\Jobs\CollectCourseSchedules;
 use App\Timetable\Jobs\InspectSchedule;
+use App\Timetable\Parsers\ParseCourseName;
+use App\Timetable\Parsers\ParseFilters;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Spatie\Regex\Regex;
 
 /**
  * @property HttpTimetableRequests $http
@@ -70,13 +50,13 @@ class SyncTimetableCommand extends Command
      *
      * @return int
      */
-    public function handle() : int
+    public function handle(): int
     {
         /**
          * Let the application know how many departments were
          * processed.
          */
-        $this->info("Build started for timetable data.");
+        $this->info('Build started for timetable data.');
 
         /**
          * The LIT web domain that stores the data we can harvest
@@ -90,7 +70,7 @@ class SyncTimetableCommand extends Command
          */
         $output = $this->output;
         $output->progressStart($filter->departments()->count());
-        $filter->departments()->map(fn($data) => get_object_vars($data))->each(function($attributes) use ($output) {
+        $filter->departments()->map(fn ($data) => get_object_vars($data))->each(function ($attributes) use ($output) {
             Department::firstOrCreate($attributes);
             $output->progressAdvance();
         });
@@ -100,8 +80,8 @@ class SyncTimetableCommand extends Command
          * Let the application know how many departments were
          * processed.
          */
-        $this->comment("Stored " . Department::count() . " out of " . $filter->departments()->count() . " departments.");
-        $this->info("Collecting course data and their associated campuses.");
+        $this->comment('Stored '.Department::count().' out of '.$filter->departments()->count().' departments.');
+        $this->info('Collecting course data and their associated campuses.');
 
         /**
          * Get all the courses in the filter, map to an array
@@ -110,14 +90,14 @@ class SyncTimetableCommand extends Command
          */
         $output = $this->output;
         $output->progressStart($filter->courses()->unique('slug')->count());
-        $filter->courses()->unique('slug')->each(function($value) use (&$output) {
+        $filter->courses()->unique('slug')->each(function ($value) use (&$output) {
             $data = (new ParseCourseName($value->name));
             try {
                 Course::firstOrCreate(['identifier' => $data->getIdentifier()], array_merge($data->toArray(), [
                     'campus_id' => Campus::firstOrCreate(['location' => $data->getLocation()])->id,
                     'department_id' => Department::firstWhere('filter', $value->filter)->id,
                 ]));
-             } catch (UnknownCourseLocationException $e) {
+            } catch (UnknownCourseLocationException $e) {
                 Log::error("Missing course data in course title {$value->name}.");
             } finally {
                 $output->progressAdvance();
@@ -129,8 +109,8 @@ class SyncTimetableCommand extends Command
          * Let the application know how many departments were
          * processed.
          */
-        $this->comment("Stored " . Course::count() . " out of " . $filter->courses()->count() . " courses belonging to " . Campus::count() . " campuses.");
-        $this->info("Collecting course schedules.");
+        $this->comment('Stored '.Course::count().' out of '.$filter->courses()->count().' courses belonging to '.Campus::count().' campuses.');
+        $this->info('Collecting course schedules.');
 
         /**
          * Get all the courses in the filter, map to an array
@@ -140,13 +120,13 @@ class SyncTimetableCommand extends Command
         $output = $this->output;
         $course = Course::all();
         $output->progressStart($course->count());
-        $course->each(function(Course $course) use ($output) {
+        $course->each(function (Course $course) use ($output) {
             InspectSchedule::dispatch($course);
             $output->progressAdvance(1);
         });
 
-        $this->comment("Schedules have been dispatched successfully to threads.");
-        $this->info("Done.");
+        $this->comment('Schedules have been dispatched successfully to threads.');
+        $this->info('Done.');
 
         /**
          * Let the command know it succeeded.
