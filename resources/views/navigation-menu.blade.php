a7453c05a2b0a0d10ab851940675cc2b1951eb11
Mark Hester <marky360@live.ie>
2021-03-31 13:33:18 +0100

    #38 Hero title font size smaller

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -18,8 +18,8 @@
                 </div>
             </div>
 
-            <div x-data class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <button dusk="search-nav" @click="$dispatch('search');" type="button" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+            <div x-data class="space-x-8 sm:-my-px sm:flex">
+                <button dusk="search-nav" @click="$dispatch('search');" type="button" class="inline-flex items-center px-1 pt-1 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 transition duration-150 ease-in-out">
                     <svg width="24" height="24" fill="none" class="mr-4 text-gray-400 transition-colors duration-200 group-hover:text-gray-500">
                         <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                     <span>Quick search for anything</span>
