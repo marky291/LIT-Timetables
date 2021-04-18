30135118a574572b0e284febca1658d170ea771d
Mark Hester <marky360@live.ie>
2021-04-18 23:03:00 +0100

    Change routing from dashboard to homepage
    # Conflicts:
    #	config/jetstream.php
    #	resources/views/homepage.blade.php
    #	resources/views/layouts/app.blade.php
    #	resources/views/navigation-menu.blade.php
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() { return view('homepage'); })->name('homepage');
+Route::get('/', function() {
+    return view('homepage');
+})->name('homepage');
+
 Route::get('/courses/{course}', [TimetableController::class, 'course'])->name('course');
 Route::get('/lecturers/{lecturer}', [TimetableController::class, 'lecturer'])->name('lecturer');
