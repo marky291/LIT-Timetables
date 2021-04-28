bdcb89a1ac733124a5722990c616194ccad2bb10
Mark Hester <Marky360@live.ie>
2021-04-28 19:56:05 +0100

    Notification of outdated timetable #54 (#55)

---
+++ database/factories/SynchronizationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Synchronization;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SynchronizationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Synchronization::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'created_at' => $this->faker->dateTimeThisMonth,
+        ];
+    }
+}
---
+++ tests/Unit/Models/SynchronizeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Synchronization;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SynchronizeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_retrieve_last_synchronized_date()
+    {
+        $date = Carbon::create(2021, 01, 01, 00, 00, 00);
+
+        Synchronization::factory()->create([
+            'created_at' => $date
+        ]);
+
+        $this->assertInstanceOf(Carbon::class, Synchronization::lastRun());
+        $this->assertEquals($date, Synchronization::lastRun());
+    }
+
+    public function test_can_retrieve_time_difference()
+    {
+        Carbon::setTestNow(Carbon::create(2021, 01, 01, 02, 00, 00));
+
+        $lastRun = Carbon::create(2021, 01, 01, 00, 00, 00);
+
+        Synchronization::factory()->create([
+            'created_at' => $lastRun
+        ]);
+
+        $this->assertEquals("2 hours ago", Synchronization::lastRun()->diffForHumans());
+    }
+
+    public function test_can_retrieve_day_difference()
+    {
+        Carbon::setTestNow(Carbon::create(2021, 01, 12, 00, 00, 00));
+
+        $lastRun = Carbon::create(2021, 01, 01, 00, 00, 00);
+
+        Synchronization::factory()->create([
+            'created_at' => $lastRun
+        ]);
+
+        $this->assertEquals(11, Synchronization::lastRun()->diff(now())->days);
+    }
+}
+
