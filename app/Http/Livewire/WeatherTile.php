a0d52cc4c6771cf8ce84b8a3f5517f1418c928e0
Mark Hester <marky360@live.ie>
2021-05-08 16:09:11 +0100

    Weather view threaded loading (defer) #43

--- app/Http/Livewire/WeatherTile.php
+++ app/Http/Livewire/WeatherTile.php
@@ -3,13 +3,17 @@
 namespace App\Http\Livewire;
 
 use Config;
+use Exception;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class WeatherTile extends Component
 {
     public string $location;
+
+    public bool $error = false;
 
     public bool $readyToLoad = false;
 
@@ -22,13 +26,17 @@ class WeatherTile extends Component
     {
         if ($this->readyToLoad)
         {
-            $api_key = Config::get('weather.api_key');
-            $location = Config::get("campus.".strtolower($this->location).".city");
-            $weather = Cache::remember($location, now()->addMinutes(5), function() use ($location, $api_key) {
-                return Http::get("api.openweathermap.org/data/2.5/weather?q={$location}, IE&appid={$api_key}&units=metric");
-            });
-
-            return view('livewire.weather-tile', ['weather' => $weather]);
+            try {
+                $api_key = Config::get('weather.api_key');
+                $location = Config::get("campus.".strtolower($this->location).".city");
+                $weather = Cache::remember($location, now()->addMinutes(5), function() use ($location, $api_key) {
+                    return Http::get("api.openweathermap.org/data/2.5/weather?q={$location}, IE&appid={$api_key}&units=metric");
+                });
+                return view('livewire.weather-tile', ['weather' => $weather]);
+            } catch (Exception $e) {
+                $this->error = true;
+                Log::error($e);
+            }
         }
 
         return view('livewire.weather-tile', ['weather' => null]);
