a068056e370cbd6847af4b5f6be953a2666619e0
Mark Hester <marky360@live.ie>
2021-05-08 19:53:50 +0100

    Homepage hero title correction

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
             <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
                 <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
                     <span class="block xl:inline dark:text-dark-text">Smarter approach to</span>
-                    <span class="block text-indigo-600 xl:inline">web timetables</span>
+                    <span class="block text-indigo-600 xl:inline">LIT Timetables</span>
                 </h1>
                 <p class="max-w-md mx-auto mt-3 text-lg text-gray-500 dark:text-dark-text sm:text-xl md:mt-5 md:max-w-3xl">
                     Built for mobile and crammed with smart features to make your day less stressful.
