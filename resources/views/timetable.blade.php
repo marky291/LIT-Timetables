e87d8c6836f8b9c98dd74375d04056dfb50300be
Mark Hester <marky360@live.ie>
2021-05-12 12:32:20 +0100

    Fix lecturer source

--- resources/views/timetable.blade.php
+++ resources/views/timetable.blade.php
@@ -45,7 +45,7 @@
 
                         @livewire('buttons.timetable-subscribe-button', ['timetable' => $model])
 
-                        @if ($model->source != null)
+                        @if ($model->identifier != null)
                             <a target="_blank" href="{{ $model->source() }}" class="mt-2 md:mt-0 dark:button dark:border-gray-600 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 View Timetable @LIT
                             </a>
