58d155aeef0deb1842c6d46270f5c86cec3c91fa
Mark Hester <marky360@live.ie>
2021-05-13 10:27:21 +0100

    Search input area now extends full length #82

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -10,7 +10,7 @@ x-on:search.window="open = true; $nextTick(() => $refs.searchbar.focus())"
         <div x-on:click.away="open = false" class="border border-transparent dark:border-gray-700 flex flex-col w-full lg:max-w-3xl h-screen lg:h-auto lg:max-h-full mx-auto bg-white dark:bg-dark-background dark:border dark:border-gray-200 lg:rounded-2xl" style="box-shadow: 0 25px 50px -12px rgba(0,0,0,.25)">
 
             <header class="flex items-center justify-between mx-6 border-b border-gray-200 dark:border-dark-border">
-                <div class="flex items-center">
+                <div class="flex items-center flex-1">
                     <div class="self-center text-indigo-700">
                         <svg wire:loading.remove class="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         <svg wire:loading class="w-7 h-7 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
@@ -21,7 +21,7 @@ x-on:search.window="open = true; $nextTick(() => $refs.searchbar.focus())"
                         x-on:keydown.arrow-down.prevent
                         wire:model="search"
                         type="text"
-                        class="h-20 text-lg border-none focus:ring-0 dark:text-dark-text dark:bg-dark-background"
+                        class="h-20 flex-1 text-lg border-none focus:ring-0 dark:text-dark-text dark:bg-dark-background"
                         placeholder="Search Courses">
                 </div>
                 <div class="hidden lg:block">
