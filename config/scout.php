fd63bac4f9256fc1dc17dd398beeccd2582ece30
Alexandru C Ipsalat <43743759+Al-Ip@users.noreply.github.com>
2021-05-17 17:11:59 +0100

    Scout installation for windows (#90)

--- config/scout.php
+++ config/scout.php
@@ -103,4 +103,14 @@ return [
         'secret' => env('ALGOLIA_SECRET', ''),
     ],
 
+
+    'mysql' => [
+        'mode' => 'LIKE',
+        'model_directories' => [app_path()],
+        'min_search_length' => 0,
+        'min_fulltext_search_length' => 4,
+        'min_fulltext_search_fallback' => 'LIKE',
+        'query_expansion' => false
+    ],
+
 ];
