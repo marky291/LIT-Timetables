737367d65aba2156f6729d1232b7e3c27226fb24
Mark Hester <Marky360@live.ie>
2021-07-06 16:04:29 +0100

    Section files

---
+++ resources/views/features.blade.php
@@ -0,0 +1,120 @@
+<div class="w-full pt-16 pb-20 mx-auto text-center max-w-7xl lg:py-48 lg:text-left">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="">
+    <div class="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+      <div>
+        <h2 class="text-base font-semibold tracking-wide text-indigo-600 uppercase">Everything you need</h2>
+        <p class="mt-2 text-3xl font-extrabold text-gray-900">All-in-one platform</p>
+        <p class="mt-4 text-lg text-gray-500">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
+      </div>
+      <div class="mt-12 lg:mt-0 lg:col-span-2">
+        <dl class="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Invite team members</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              You can manage phone, email and chat conversations all from a single mailbox.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">List view</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              You can manage phone, email and chat conversations all from a single mailbox.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Keyboard shortcuts</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              You can manage phone, email and chat conversations all from a single mailbox.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Calendars</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              You can manage phone, email and chat conversations all from a single mailbox.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Notifications</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              Find what you need with advanced filters, bulk actions, and quick views.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Boards</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              Find what you need with advanced filters, bulk actions, and quick views.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Reporting</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              Find what you need with advanced filters, bulk actions, and quick views.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <!-- Heroicon name: outline/check -->
+              <svg class="absolute w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+              </svg>
+              <p class="text-lg font-medium leading-6 text-gray-900 ml-9">Mobile app</p>
+            </dt>
+            <dd class="mt-2 text-base text-gray-500 ml-9">
+              Find what you need with advanced filters, bulk actions, and quick views.
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+</div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -18,7 +18,7 @@
                         </button>
                     </div>
                     <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                        <a dusk="href-learnmore" target="_blank" href="https://github.com/marky291/lit-timetables" class="dark:button flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-600 bg-white h-full rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+                        <a dusk="href-learnmore" target="_blank" href="https://github.com/marky291/lit-timetables" class="flex items-center justify-center w-full h-full px-8 py-3 text-base font-medium text-indigo-600 bg-white rounded-md dark:button hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
                             <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 mr-2">
                                 <path d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"></path>
                             </svg> {{ __('Open Source') }}
@@ -35,5 +35,11 @@
             <img class="absolute inset-0 object-cover w-full h-full" src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2102&amp;q=80" alt="">
         </div>
     </main>
+
+    @include('statistics')
+
+    @include('features')
+
+    @include('about')
 
 </x-app-layout>
